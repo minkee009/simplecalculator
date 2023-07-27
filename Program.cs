c1a321f0ac10f6a5fe551298907465a6801415e6
minkee5004 <minkee5004@gmail.com>
2023-07-27 09:58:21 +0900

    음수 양수 기능추가

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace SimpleCalculator
 {
     internal class Program
     {
+        static int[] isPostive = new int[2];
+
         static void Main(string[] args)
         {
             string inputString = Console.ReadLine();
@@ -16,11 +19,27 @@ namespace SimpleCalculator
             int number1 = 0;
             int number2 = 0;
             int xoperator = 0;
-
             int count = 0;
+            int errorCount = 0;
 
+            isPostive[0] = 1;
+            isPostive[1] = 1;
 
             for (int i = 0; i < nonSpaceinputString.Length; i++)
+            {
+                if (nonSpaceinputString[i] >= 48 && nonSpaceinputString[i] <= 57)
+                {
+                    break;
+                }
+                int changeInt = nonSpaceinputString[i];
+
+                if (changeInt == 45) isPostive[0] = -1;
+                if (changeInt == 43) isPostive[0] = 1;
+
+                errorCount++;
+            }
+
+            for (int i = errorCount; i < nonSpaceinputString.Length; i++)
             {
                 if (nonSpaceinputString[i] >= 48 && nonSpaceinputString[i] <= 57)
                 {
@@ -60,16 +79,35 @@ namespace SimpleCalculator
 
             var savedCount = count;
 
-            for (int i = 0; i < savedCount; i++)
+            for (int i = errorCount; i < savedCount + errorCount; i++)
             {
                 int num = nonSpaceinputString[i] - 48;
                 number1 += num * (int)Math.Pow(10, (double)count - 1);
                 count--;
+               
             }
-
             //
 
             var lastcount = savedCount + 1;
+
+            errorCount = 0;
+
+            for (int i = lastcount; i < nonSpaceinputString.Length; i++)
+            {
+                if (nonSpaceinputString[i] >= 48 && nonSpaceinputString[i] <= 57)
+                {
+                    break;
+                }
+                int changeInt = nonSpaceinputString[i];
+
+                if (changeInt == 45) isPostive[1] = -1;
+                if (changeInt == 43) isPostive[1] = 1;
+
+                errorCount++;
+            }
+
+            lastcount += errorCount;
+
             count = 0;
 
             for (int i = lastcount; i < nonSpaceinputString.Length; i++)
@@ -99,13 +137,13 @@ namespace SimpleCalculator
             switch (mode)
             {
                 case 1:
-                    return number1 + number2;
+                    return (isPostive[0] * number1) + (isPostive[1] * number2);
                 case 2:
-                    return number1 - number2;
+                    return (isPostive[0] * number1) - (isPostive[1] * number2);
                 case 3:
-                    return number1 * number2;
+                    return (isPostive[0] * number1) * (isPostive[1] * number2);
                 case 4:
-                    return number1 / number2;
+                    return (isPostive[0] * number1)  / (isPostive[1] * number2);
             }
             return 0;
         }
